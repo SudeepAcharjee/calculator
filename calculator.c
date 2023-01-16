6390fb71d0b84e6559bae2c287ae5d4251cd9ef0
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-01-16 20:49:39 +0530

    Code

---
+++ calculator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    char operator;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2f + %.2f = %.2f", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2f - %.2f = %.2f", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%.2f * %.2f = %.2f", num1, num2, result);
+            break;
+        case '/':
+            result = num1 / num2;
+            printf("%.2f / %.2f = %.2f", num1, num2, result);
+            break;
+        default:
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
+
